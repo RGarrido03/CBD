413f2901370b2120ab6888e194b3cfe2f9dcdd52
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-09-24 02:37:35 +0100

    Initial exercise 6 files

---
+++ Guiao-1/ex6/src/main/java/pt/ua/deti/cbd/App.java
@@ -0,0 +1,14 @@
+package pt.ua.deti.cbd;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App
+{
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        logger.info("Hello World!");
+    }
+}
