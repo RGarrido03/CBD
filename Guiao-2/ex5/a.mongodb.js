7234bfb6c8fe006b5c956c80badc8901425991fd
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-10-26 23:47:16 +0100

    Make 2.5 d) work inside mongodb

--- Guiao-2/ex5/a.mongodb.js
+++ Guiao-2/ex5/a.mongodb.js
@@ -1,18 +1,9 @@
-function capicua() {
-  let capicuaArray = [];
-
-  db.phones.find().forEach(function (doc) {
-    let n = doc["components"]["number"].toString();
-
-    for (i = 0; i < Math.floor(n.length / 2); i++) {
-      if (n[i] != n[n.length - i - 1]) {
-        break;
-      }
-      if (i == Math.floor(n.length / 2) - 1) {
-        capicuaArray.push(n);
-      }
+function capicua(components) {
+  let n = components.number.toString();
+  for (let i = 0; i < Math.floor(n.length / 2); i++) {
+    if (n[i] != n[n.length - i - 1]) {
+      return false;
     }
-  });
-
-  return capicuaArray;
+  }
+  return true;
 }
